3a842a80c87a4ef83ee489b3d505a56d4dd4ae7e
Rakul <rabrosrajgopu@gmail.com>
2023-03-07 18:45:15 +0100

    changed layout

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,7 +1,39 @@
-<form method="POST" action="">
-    <input type="email" name="email" placeholder="Email"><br>
-    <input type="text" name="last_name" placeholder="Name"><br>
-    <input type="text" name="first_name" placeholder="Vorname"><br>
-    <input type="text" name="comments" placeholder="Bemerkungen"><br>
-    <button type="submit">Speichern</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KamUni</title>
+</head>
+
+<body>
+
+    <form method="POST" action="">
+
+        <div>
+            <input type="email" name="email" placeholder="Email"><br>
+        </div>
+
+        <div>
+            <input type="text" name="last_name" placeholder="Name"><br>
+        </div>
+
+        <div>
+            <input type="text" name="first_name" placeholder="Vorname"><br>
+        </div>
+        <div>
+            <textarea name="message" placeholder="Schreiben Sie eine Nachricht"></textarea>
+        </div>
+
+        <button type="submit">Speichern</button>
+
+    </form>
+
+
+
+
+</body>
+
+</html>
